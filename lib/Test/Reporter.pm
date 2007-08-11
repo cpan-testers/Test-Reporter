868951ec9c84f4ac62e4e52e3a59c6b8091eadb2
Adam J. Foxson <afoxson@pobox.com>
2007-08-11 14:08:02 -0400

    Herein lies Test-Reporter-1.29_03.

--- lib/Test/Reporter.pm
+++ lib/Test/Reporter.pm
@@ -1,4 +1,4 @@
-# $Id: Reporter.pm 60 2007-06-26 20:57:10Z afoxson $
+# $Id: Reporter.pm 64 2007-06-27 07:27:32Z afoxson $
 # $HeadURL: https://test-reporter.googlecode.com/svn/branches/1.30/lib/Test/Reporter.pm $
 
 # Test::Reporter - sends test results to cpan-testers@perl.org
@@ -27,7 +27,7 @@ use constant FAKE_NO_NET_DNS => 0;    # for debugging only
 use constant FAKE_NO_NET_DOMAIN => 0; # for debugging only
 use constant FAKE_NO_MAIL_SEND => 0;  # for debugging only
 
-$VERSION = '1.29_02';
+$VERSION = '1.29_03';
 
 local $^W = 1;
 
