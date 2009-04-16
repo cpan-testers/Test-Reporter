2e14b94feb4adc9a537e10c7c8ca9df299597843
David Golden <dagolden@cpan.org>
2009-04-16 12:26:46 -0400

    bump version to 1.53_03

--- lib/Test/Reporter.pm
+++ lib/Test/Reporter.pm
@@ -34,7 +34,7 @@ use constant FAKE_NO_NET_DNS => 0;    # for debugging only
 use constant FAKE_NO_NET_DOMAIN => 0; # for debugging only
 use constant FAKE_NO_MAIL_SEND => 0;  # for debugging only
 
-$VERSION = '1.53_02';
+$VERSION = '1.53_03';
 
 local $^W = 1;
 
