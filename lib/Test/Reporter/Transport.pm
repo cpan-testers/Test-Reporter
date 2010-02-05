2fb4661fe080ae813d782fa6fdb3858891d0e671
David Golden <dagolden@cpan.org>
2010-02-04 22:10:02 -0500

    Bump version to 1.55

--- lib/Test/Reporter/Transport.pm
+++ lib/Test/Reporter/Transport.pm
@@ -2,7 +2,7 @@ use strict;
 BEGIN{ if (not $] < 5.006) { require warnings; warnings->import } }
 package Test::Reporter::Transport;
 use vars qw/$VERSION/;
-$VERSION = '1.54';
+$VERSION = '1.55';
 $VERSION = eval $VERSION;
 
 sub new     { die "Not implemented" }
