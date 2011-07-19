dc1997646653a9d71882f3f361e9cb8d9bd5a7ed
David Golden <dagolden@cpan.org>
2011-07-19 10:05:52 -0400

    Add a Null transport class

---
+++ lib/Test/Reporter/Transport/Null.pm
@@ -0,0 +1,41 @@
+use strict;
+BEGIN{ if (not $] < 5.006) { require warnings; warnings->import } }
+package Test::Reporter::Transport::Null;
+# VERSION
+
+use base 'Test::Reporter::Transport';
+
+sub new {
+  return bless {}, shift;
+}
+
+sub send {
+  return 1; # do nothing
+}
+
+1;
+
+# ABSTRACT: Null transport for Test::Reporter
+
+__END__
+
+=for Pod::Coverage new send
+
+=head1 SYNOPSIS
+
+    my $report = Test::Reporter->new(
+        transport => 'Null',
+    );
+
+=head1 DESCRIPTION
+
+This module provides a "null" transport option that does nothing when
+C<send()> is called.
+
+=head1 USAGE
+
+See L<Test::Reporter> and L<Test::Reporter::Transport> for general usage
+information.
+
+=cut
+
